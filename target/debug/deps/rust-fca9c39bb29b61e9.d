327d2d2953ed450a4e54095ab416edee83b16776
skrevolve <skrevolve@naver.com>
2022-02-09 14:04:00 +0900

    #1 upload

---
+++ target/debug/deps/rust-fca9c39bb29b61e9.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/User/Desktop/Rust/rust/target/debug/deps/rust-fca9c39bb29b61e9.rmeta: src/main.rs
+
+/mnt/c/Users/User/Desktop/Rust/rust/target/debug/deps/rust-fca9c39bb29b61e9.d: src/main.rs
+
+src/main.rs:
